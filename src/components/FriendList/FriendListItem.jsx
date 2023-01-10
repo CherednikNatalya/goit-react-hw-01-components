cb3e245f972fc305336be403c9abc9bded88a81c
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2023-01-10 11:19:43 +0200

    12

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,13 +1,12 @@
 import PropTypes from 'prop-types';
-// import { GrStatusGoodSmall } from 'react-icons/fa';
+import css from './FriendList.module.css'
 
 export const FriendListItem = ({avatar, name, isOnline, id}) => {
 return (
-    <li class="item">
-      {/* <GrStatusGoodSmall size={16}/> */}
-  <span class="status">{isOnline}</span>
-  <img class="avatar" src={avatar} alt="User avatar" width="48" />
-  <p class="name">{name}</p>
+    <li className={css.item}>
+  <span className={isOnline ? css.statusOnline : css.statusOffline}></span>
+  <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+  <p className={css.name}>{name}</p>
 </li>
 )
 }
