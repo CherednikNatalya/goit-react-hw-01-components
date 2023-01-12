4a51410267bd85af3c01af3fe40472f66ed54ef9
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2023-01-12 09:45:12 +0200

    45

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -5,7 +5,7 @@ import css from './FriendList.module.css'
 
 export const FriendList = ({friends}) => {
     return (
-        <ul className={css.friend_list}>
+        <ul className = {css.friend_list}>
         {friends.map(({avatar, name, isOnline, id}) => (
           <FriendListItem 
           key={id}
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,6 +1,5 @@
 import {App} from '../App'
 import PropTypes from 'prop-types';
-import {TransactionItem} from './TransactionItem'
 import css from './TransactionHistory.module.css'
 
 export const TransactionHistory =({items}) => {
@@ -15,14 +14,15 @@ return (
     </tr>
   </thead> 
   <tbody>
-  {items.map(({id, type, amount, currency}) => (
-          <TransactionItem
-          key={id}
-          type ={type} 
-          amount={amount} 
-          currency={currency}
-          />
-        ))}
+  {items.map(({id, type, amount, currency}) => {
+         return (
+                        <tr key={id}>
+                          <td>{type}</td>
+                          <td>{amount}</td>
+                          <td>{currency}</td>
+                        </tr>
+                      );
+         })}
   </tbody>
   </table>
   </div>
@@ -46,41 +46,4 @@ TransactionHistory.propTypes ={
 
 
 
-  // export const TransactionHistory = ({ items }) => {
-  //   return (
-  //     <div className={css.wrapper}>
-  //       <table className={css.transaction_history}>
-  //         <thead>
-  //           <tr>
-  //             <th>Type</th>
-  //             <th>Amount</th>
-  //             <th>Currency</th>
-  //           </tr>
-  //         </thead>
-  //         <tbody>
-  //           {items.map(event => {
-  //             return (
-  //               <tr key={event.id}>
-  //                 <td>{event.type}</td>
-  //                 <td>{event.amount}</td>
-  //                 <td>{event.currency}</td>
-  //               </tr>
-  //             );
-  //           })}
-  //         </tbody>
-  //       </table>
-  //     </div>
-  //   );
-  // };
-  
-  // TransactionHistory.propTypes = {
-  //   transactions: PropTypes.arrayOf(
-  //     PropTypes.shape({
-  //       id: PropTypes.number.isRequired,
-  //       type: PropTypes.string.isRequired,
-  //       amount: PropTypes.string.isRequired,
-  //       currency: PropTypes.string.isRequired,
-  //     })
-  //   ),
-  // };
   
